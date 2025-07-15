b06701b5b48fa66e614b05dc9ba9288c95c5af3d
Колесник Сергей <00volchenok0013@mail.ru>
2025-07-15 12:09:35 +0300

    update views.py create urls.py, create templates

--- blog/admin.py
+++ blog/admin.py
@@ -13,6 +13,8 @@ class CategoryAdmin(DjangoMpttAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
 
 
--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Blog2Config(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog_2'
+    name = 'blog'
